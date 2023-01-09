71be8921e53f331679d0a0ecf8602b3599c4bf0e
Sara-Mill <106774866=sarbear2-0@users.noreply.github.com>
2023-01-09 14:19:03 -0700

    upated contact form

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -3,50 +3,12 @@ import { Form, Input, TextArea, Button } from "semantic-ui-react";
 import "./portfolio.css";
 
 const Contact = () => {
-  const handleOnSubmit = {};
+  
 
   return (
     <div className="Contact">
-      <h4>
-        Direct email is coming Soon! For now, please use the links below to
-        connect via Github, LinkedIn, and StackOverflow. Thank you!
-      </h4>
-      <Form onSubmit={handleOnSubmit}>
-        <Form.Field
-          class="form-field"
-          id="form-input-control-email"
-          control={Input}
-          label="Email"
-          name="user_email"
-          placeholder="Email…"
-          required
-          icon="mail"
-          iconPosition="left"
-        />
-        <Form.Field
-          class="form-field"
-          id="form-input-control-last-name"
-          control={Input}
-          label="Name"
-          name="user_name"
-          placeholder="Name…"
-          required
-          icon="user circle"
-          iconPosition="left"
-        />
-        <Form.Field
-          class="form-field"
-          id="form-textarea-control-opinion"
-          control={TextArea}
-          label="Message"
-          name="user_message"
-          placeholder="Message…"
-          required
-        />
-        <Button disabled type="submit" color="green">
-          Submit
-        </Button>
-      </Form>
+      
+      <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdEg5mxmdlEJYyWROGcT7u4izedSC1Z39Auyy-e1YSko-MFKw/viewform?embedded=true" width="640" height="947" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
     </div>
   );
 };
