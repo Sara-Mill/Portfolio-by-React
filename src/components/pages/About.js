f025aa6e40d9a47eb7d55a4c538007fbd098d3f0
Sara-Mill <106774866=sarbear2-0@users.noreply.github.com>
2023-01-20 16:17:16 -0700

    updated about page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -6,7 +6,7 @@ export default function About() {
       <h1>About SaraMarie</h1>
      
       <p className = 'about'>
-       I am a lifelong learner with a background in Montessori education.  Currently enrolled in a Full Stack Web Development Bootcamp with an expected completion date of January 9th, 2023.  My goal is to inspire curiosity through websites that motivate, provide information,  and cultivate wonder.  Let's Connect!    </p>
+       I am a lifelong learner with a background in Montessori education.  Recenty completed a Full Stack Web Development Bootcamp on January 9th, 2023.  My goal is to inspire curiosity through websites that motivate, provide information,  and cultivate wonder.  Let's Connect!    </p>
     </div>
   );
 }
