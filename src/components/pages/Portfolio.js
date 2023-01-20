9705ddede0ea7ac7b0822eba70ed05892af56c91
Sara-Mill <106774866=sarbear2-0@users.noreply.github.com>
2023-01-20 15:29:03 -0700

    updated links on portfolio page

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -15,13 +15,22 @@ export default function About() {
       <h1>Portfolio </h1>
       <div className="images-container-outer">
         <div className="images-container1">
-        
           <div>
-            <a href="https://github.com/Sara-Mill/Artist-Profile" target="_blank" rel="noreferrer">
+            <a
+              href="https://github.com/Sara-Mill/Artist-Profile"
+              target="_blank"
+              rel="noreferrer"
+            >
               <FaGithub color="black" size="34px" />
             </a>
-            <a href="https://pwa-jate-mystic-app.herokuapp.com" target="_blank" rel="noreferrer">
-              <p className="project-links">Collaboration on a REST API: Artist-Profile</p>
+            <a
+              href="https://sara-mill.github.io/Artist-Profile/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <p className="project-links">
+                Collaboration on a REST API: Artist-Profile
+              </p>
               <img
                 src={KYA}
                 alt="Black background showing with list of top 5 tracks on the left, and a youtube video by the artist on the right."
@@ -29,115 +38,129 @@ export default function About() {
               ></img>
             </a>
           </div>
+        </div>
+
+        <div className="images-container">
+          <div>
+            <a
+              href="https://github.com/Sara-Mill/PWA-text-editor"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FaGithub color="black" size="34px" />
+            </a>
+            <a
+              href="https://pwa-jate-mystic-app.herokuapp.com"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <p className="project-links">Progressive Web Application</p>
+              <img
+                src={JATE}
+                alt="Black background, with text editor in yellow."
+                className="project-thumbnails"
+              ></img>
+            </a>
           </div>
-          
-          <div className="images-container">
-        
+        </div>
+
+        <div className="images-container">
+          <div>
+            <a
+              href="https://github.com/LizBailey75/DEAR-Reader"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FaGithub color="black" size="34px" />
+            </a>
+            <a
+              href="https://dear-reader-22-app.herokuapp.com/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <p className="project-links">
+                Collaboration on a SQL Database: Drop Everything and Read
+              </p>
+              <img
+                src={DEAR}
+                alt="book opening up spreading light"
+                className="project-thumbnails"
+              ></img>
+            </a>
+          </div>
+        </div>
+
         <div>
-          <a href="https://github.com/Sara-Mill/PWA-text-editor" target="_blank" rel="noreferrer">
+          <a
+            href="https://github.com/Sara-Mill/New-Password-Generator"
+            target="_blank"
+            rel="noreferrer"
+          >
             <FaGithub color="black" size="34px" />
           </a>
-          <a href="https://pwa-jate-mystic-app.herokuapp.com" target="_blank" rel="noreferrer">
-            <p className="project-links">Progressive Web Application</p>
+          <a
+            href="https://sara-mill.github.io/New-Password-Generator/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <p className="project-links">
+              Vanilla Javascript: Password Generator
+            </p>
             <img
-              src={JATE}
-              alt="Black background, with text editor in yellow."
+              src={PW_G}
+              alt="white background with dashed boxes displaying generated password, and a red button below"
               className="project-thumbnails"
             ></img>
           </a>
-          </div>
         </div>
-     
-        
-          <div className="images-container">
-            
-            
-              <div>
-                <a href="https://github.com/LizBailey75/DEAR-Reader" target="_blank" rel="noreferrer">
-                  <FaGithub color="black" size="34px" />
-                </a>
-                <a href="https://dear-reader-22-app.herokuapp.com/" target="_blank" rel="noreferrer">
-                  <p className="project-links">Collaboration on a SQL Database: Drop Everything and Read</p>
-                  <img
-                    src={DEAR}
-                    alt="book opening up spreading light"
-                    className="project-thumbnails"
-                  ></img>
-                </a>
-              
-              </div>
-            </div>
-            <div className="images-container">
-            
-           
-              <div>
-                <a href="https://github.com/drewrvaz/dev-team-launch" target="_blank" rel="noreferrer">
-                  <FaGithub color="black" size="34px" />
-                </a>
-                <a href="https://dev-team-launch.herokuapp.com/" target="_blank" rel="noreferrer">
-                  <p className="project-links">Collaboration on a MERN Application: Team Creator</p>
-                  <img
-                    src={CHOICE}
-                    alt="grey background with wind blowing snow off the top of a mountain"
-                    className="project-thumbnails"
-                  ></img>
-                </a>
-              
-             </div>
-            </div>
-            <div>
-                <a href="https://github.com/Sara-Mill/New-Password-Generator" target="_blank" rel="noreferrer">
-                  <FaGithub color="black" size="34px" />
-                </a>
-                <a href="https://sara-mill.github.io/New-Password-Generator/" target="_blank" rel="noreferrer">
-                  <p className="project-links">Vanilla Javascript: Password Generator</p>
-                  <img
-                    src={PW_G}
-                    alt="white background with dashed boxes displaying generated password, and a red button below"
-                    className="project-thumbnails"
-                  ></img>
-                </a>
-              
-             </div>
-            </div>
-            <div className="images-container">
-           
-            
-              <div>
-                <a href="https://github.com/Sara-Mill/random_name_generator" target="_blank" rel="noreferrer">
-                  <FaGithub color="black" size="34px" />
-                </a>
-                <a href="https://random-name-generator.herokuapp.com/" target="_blank" rel="noreferrer">
-                  <p className="project-links">React Random Name Generator</p>
-                  <img
-                    src={RNG}
-                    alt="back background with large blue react icon spinning in center, and a toggle bar at top"
-                    className="project-thumbnails"
-                  ></img>
-                </a>
-              </div>
-              
-            </div>
-           
-            <div className="images-container">
-        
-      
-            <div>
-              <a href="https://github.com/Sara-Mill/Social_Network_API_NoSQL" target="_blank" rel="noreferrer">
-                <FaGithub color="black" size="34px" />
-              </a>
-              <a href="https://github.com/Sara-Mill/Social_Network_API_NoSQL" target="_blank" rel="noreferrer">
-                <p className="project-links">Social Network API NoSQL</p>
-                <img
-                  src={SNAPI}
-                  alt="Black background showing table of employees in white."
-                  className="project-thumbnails"
-                ></img>
-              </a>
-            </div>
-            </div>
-            </div>   
-     
-  
+      </div>
+      <div className="images-container">
+        <div>
+          <a
+            href="https://github.com/Sara-Mill/random_name_generator"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FaGithub color="black" size="34px" />
+          </a>
+          <a
+            href="https://random-name-generator.herokuapp.com/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <p className="project-links">React Random Name Generator</p>
+            <img
+              src={RNG}
+              alt="back background with large blue react icon spinning in center, and a toggle bar at top"
+              className="project-thumbnails"
+            ></img>
+          </a>
+        </div>
+      </div>
+
+      <div className="images-container">
+        <div>
+          <a
+            href="https://github.com/Sara-Mill/Social_Network_API_NoSQL"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FaGithub color="black" size="34px" />
+          </a>
+          <a
+            href="https://github.com/Sara-Mill/Social_Network_API_NoSQL"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <p className="project-links">Social Network API NoSQL</p>
+            <img
+              src={SNAPI}
+              alt="Black background showing table of employees in white."
+              className="project-thumbnails"
+            ></img>
+          </a>
+        </div>
+      </div>
+    </div>
   );
 }
